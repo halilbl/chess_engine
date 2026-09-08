f82feab7f6906761d9e7ee1f10bb6125cc2e54e2
Halil Leblebici <halilleblebici3@gmail.com>
2026-09-09 00:16:02 +0300

    added catch 2

--- out/build/x64-Debug/_deps/catch2-subbuild/cmake_install.cmake
+++ out/build/x64-Debug/_deps/catch2-subbuild/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/blebl/Desktop/repos/chess_engine
+# Install script for directory: C:/Users/blebl/Desktop/repos/chess_engine/out/build/x64-Debug/_deps/catch2-subbuild
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/blebl/Desktop/repos/chess_engine/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/catch2-populate")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -32,15 +32,10 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("C:/Users/blebl/Desktop/repos/chess_engine/out/build/x64-Debug/_deps/catch2-build/cmake_install.cmake")
-endif()
-
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/blebl/Desktop/repos/chess_engine/out/build/x64-Debug/install_local_manifest.txt"
+  file(WRITE "C:/Users/blebl/Desktop/repos/chess_engine/out/build/x64-Debug/_deps/catch2-subbuild/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/blebl/Desktop/repos/chess_engine/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/blebl/Desktop/repos/chess_engine/out/build/x64-Debug/_deps/catch2-subbuild/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
