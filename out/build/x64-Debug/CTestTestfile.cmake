2ceefcedcbe4d2f0468d57ee961ac60e95385ab0
Halil Leblebici <halilleblebici3@gmail.com>
2026-09-10 07:06:17 +0300

    Ignore build output and scratch file

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/blebl/Desktop/repos/chess_engine
-# Build directory: C:/Users/blebl/Desktop/repos/chess_engine/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ChessTests "C:/Users/blebl/Desktop/repos/chess_engine/out/build/x64-Debug/chess_tests.exe")
-set_tests_properties(ChessTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/blebl/Desktop/repos/chess_engine/CMakeLists.txt;22;add_test;C:/Users/blebl/Desktop/repos/chess_engine/CMakeLists.txt;0;")
-subdirs("_deps/catch2-build")
